c1befba5a41055bde26482a4a0f6a8dbc61fd8da
dilmyImesha <162699742+dilmyImesha@users.noreply.github.com>
2025-12-14 15:53:14 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import AppNavigator from './navigation/AppNavigator';
+
+import './global.css';
+
+export default function App() {
+  return (
+    <>
+      <AppNavigator />
+      <StatusBar style="auto" />
+    </>
+  );
+}
